f5cf12a959af40c32a11df3726c0e15fe4e6dc41
Narimasa Sakurai <whiteonyx@gmail.com>
2025-06-24 17:32:40 +0900

    clean example

--- example/initdb.d/init.sql
+++ example/initdb.d/init.sql
@@ -1,8 +1,3 @@
--- INSTALL bigquery FROM community;
--- LOAD bigquery;
-
--- ATTACH 'project=cloudandbuild dataset=analytics_marts_prd' AS bq (TYPE bigquery, READ_ONLY);
-
 -- install postgres;
 -- load postgres;
 
@@ -20,10 +15,5 @@ load httpfs;
 --     USE_SSL false,
 --     URL_STYLE 'path'
 -- );
--- CREATE OR REPLACE VIEW s3_test AS SELECT * FROM parquet_scan('s3://analytics/data/dbt.parquet');
-
-CREATE OR REPLACE VIEW my_table AS SELECT * FROM parquet_scan('./example/data/my_table.parquet');
-
-CREATE OR REPLACE VIEW frameworks AS SELECT * FROM read_json('./example/data/frameworks.json');
 
 CREATE OR REPLACE VIEW japan_birth AS SELECT * FROM read_csv('./example/data/japan_birth.csv');
